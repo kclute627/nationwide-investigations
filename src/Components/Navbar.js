51d72e770e7278959968505cf5025d8f47e9c1bc
Kyle C <kyclutter@gmail.com>
2019-10-23 10:56:53 -0500

    updates

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from '../assets/logo/logo-large.png'
 
 class Navbar extends Component {
 
@@ -11,6 +12,17 @@ class Navbar extends Component {
 
                 </div>
                 <div className="navbar__bottom">
+                    <a href="wwww.nationwide-investigations.com" className="navbar__bottom-logo">
+                        <img src={logo} alt=""/>
+                    </a>
+                    <div className="navbar__bottom-nav">
+                        <ul>
+                            <li>Home</li>
+                            <li>Services</li>
+                            <li>About</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
 
                 </div>
 
