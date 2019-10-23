c13624d525ac39c6a8ed907f3ba13f2105ada241
Kyle C <kyclutter@gmail.com>
2019-10-23 10:39:25 -0500

    updates

---
+++ src/Components/Navbar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+
+    render(){
+
+
+        return(
+            <div className="navbar">
+                <div className="navbar__top">
+
+                </div>
+                <div className="navbar__bottom">
+
+                </div>
+
+            </div>
+        )
+    }
+}
+
+
+
+export default Navbar;
