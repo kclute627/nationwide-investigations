cbb2d27c4e0dfb301c4cde8ee3d48b9669bd0676
Kyle C <kyclutter@gmail.com>
2019-11-04 15:28:21 -0600

    more updates

---
+++ src/Components/ServiceBOx.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactSvg from 'react-svg';
+
+
+
+const ServicesBox = () => {
+
+    return(
+        <div className="services__box-box">
+
+        </div>
+    )
+
+}
+
+
+export default ServicesBox;
