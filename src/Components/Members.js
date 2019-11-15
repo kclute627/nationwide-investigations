be3d352efa3954d6143e936d5ac8de7d28a5fa44
Kyle C <kyclutter@gmail.com>
2019-11-15 16:06:42 -0600

    updates

--- src/Components/Members.js
+++ src/Components/Members.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ScrollAnimation from "react-animate-on-scroll";
 import naaps from "../assets/naaps.png";
 import ilaaps from "../assets/ilaaps.png";
-import adsi from "../assets/adsi.png";
 
 class Members extends Component {
+  render() {
+    return (
+      <div className="members__container">
+        <div className="members__header">Affiliations</div>
 
-    render(){
+        <div className="members__img">
+            <ScrollAnimation animateIn="slide">
+                <a href="https://napps.org/" className="b">
+            <img
+              src={naaps}
+              alt="National Association of Private Process servers"
+              className="members__img-1 a"
+            />
+          </a>
+            </ScrollAnimation>
+          
 
-        return(
-            <div className="members__container">
-                <div className="members__header">
-                Affiliations
-                </div>
-                <div className="members__img">
-                    <a href="https://napps.org/">
-                        <img src={naaps} alt="National Association of Private Process servers" className="members__img-1"/>
-                    </a>
-                    <a href="http://ilapps.com/">
-                        <img src={ilaaps} alt="" className="members__img-2"/>
-                    </a>
-                    
-                   
-                </div>
-
-            </div>
-        )
-    }
+          <a href="http://ilapps.com/">
+            <img src={ilaaps} alt="" className="members__img-2" />
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
-
-
 
 export default Members;
