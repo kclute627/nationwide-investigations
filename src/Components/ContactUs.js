f3a740e4ae7d949c8a4a95ab64443263cd17e4bd
Kyle C <kyclutter@gmail.com>
2019-11-19 08:38:01 -0600

    updates

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -7,6 +7,12 @@ class ContactUs extends Component {
 
         return(
             <div className="contactus__container">
+                <div className="contactus__header">Contact Us</div>
+                <div className="contactus__contact">
+                    <h1 className="contactus__contact-header">
+                        Send Us a Message
+                    </h1>
+                </div>
 
             </div>
         )
