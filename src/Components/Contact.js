1c661283ad52ef2800ff5652fb5cbe1771723aeb
Kyle C <kyclutter@gmail.com>
2019-12-02 16:45:35 -0600

    updates

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,4 +1,5 @@
 import React , { Component } from 'react';
+import {Helmet} from 'react-helmet';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import ContactUs from './ContactUs';
@@ -13,6 +14,16 @@ class Contact extends Component{
 
         return(
             <div className="contact__container">
+                  <Helmet>
+          <meta charSet="utf-8" />
+          <meta
+            name="keywords"
+            content="Process Server, Chicago process Server, process serving, Illinois Process Server, Same-day Process Service"
+          />
+          <meta name='description' content='Contact Us if you need a process server in Illinois'/>
+          <title>Process Server // Contact Us </title>
+         
+        </Helmet>
 
                 <Navbar contact ="true"/>
                 <ContactUs/>
