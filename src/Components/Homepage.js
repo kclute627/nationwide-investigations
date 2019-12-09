6eca9a3cbcf745798a661288ba0cda838d89c9f6
Kyle C <kyclutter@gmail.com>
2019-12-09 10:16:03 -0600

    updates

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import ReactSVG from 'react-svg';
+import phone1 from '../assets/icons/phone.svg';
+ 
 
 
 
@@ -17,6 +19,11 @@ class Homepage extends Component {
                  <div className="homepage__container-header">
                   <h1>Trusted Process Servers </h1>
                   <h4>Serving all of Illinois and Headquarted in Chicago, IL </h4>
+                  <div className="phone-box">
+                      <div className="phone-box-1">Serving Illinois</div>
+                      
+                      <a href="href=tel:3123613598" className="phone-box-3"> <ReactSVG  src={phone1} /> 312-361-3598 </a>
+                  </div>
                   <Link to='/contact' className="btn-left"> <span className="span__btn-1">Get A Free Quote</span></Link>             
                   
                 </div>
