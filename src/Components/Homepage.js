b7fadc9f2e10fad6d3ea5e732e433df13feab93c
Kyle C <kyclutter@gmail.com>
2019-12-10 09:10:45 -0600

    updates

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ReactSVG from 'react-svg';
 import phone1 from '../assets/icons/phone.svg';
+import mail from '../assets/icons/mail.svg';
+import home from '../assets/icons/home.svg';
  
 
 
@@ -15,18 +17,30 @@ class Homepage extends Component {
 
         return (
             <div className="homepage__container">
+
                
                  <div className="homepage__container-header">
                   <h1>Trusted Process Servers </h1>
                   <h4>Serving all of Illinois and Headquarted in Chicago, IL </h4>
-                  <div className="phone-box">
-                      <div className="phone-box-1">Serving Illinois</div>
-                      
-                      <a href="href=tel:3123613598" className="phone-box-3"> <ReactSVG  src={phone1} /> 312-361-3598 </a>
-                  </div>
+                  
                   <Link to='/contact' className="btn-left"> <span className="span__btn-1">Get A Free Quote</span></Link>             
                   
                 </div>
+                <div className="phone-box">
+                      <div className="phone-box-22">Process Servers</div>
+                      <div className="phone-box-1">Serving Illinois</div>
+                      <div className="phone-box-info">
+                          
+                      </div>
+                      <a href="href=malto:info@nationwide-investigations.com" className="phone-box-3">EMAIL</a>
+                      <div className="phone-box-5">  116 W Jackson <br/> Suite #254 <br/>Chicago, IL 60604 </div>
+                      
+                      <a href="href=tel:3123613598" className="phone-box-4">  312-361-3598 </a>
+                      <ReactSVG  src={home} className='svg-1'/>
+                      <ReactSVG  src={phone1} className='svg-2'/>
+                      <ReactSVG  src={mail} className='svg-3'  />
+
+                  </div>
                 
                 
 
